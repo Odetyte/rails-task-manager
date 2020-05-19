b99daf00c7998627af77d2f0e65d14e58c2e776a
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-05-19 17:29:40 +0200

    with no scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
+
+# As a user, I can list tasks
+# As a user, I can view the details of a task
+# As a user, I can add a new task
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a tas
--- db/migrate/20200519122640_create_tasks.rb
+++ db/migrate/20200519122640_create_tasks.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
